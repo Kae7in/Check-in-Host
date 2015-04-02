8ab5ea2b9c70aaa95e8aefd1895ed236d6bcf2f2
Kaelin <kae7in@gmail.com>
2015-04-02 11:54:24 -0500

    Initial Commit

---
+++ Check-In Host/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Check-In Host
+//
+//  Created by Kaelin D. Hooper on 4/2/15.
+//  Copyright (c) 2015 CS378. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Check-In HostTests/Check_In_HostTests.m
@@ -0,0 +1,40 @@
+//
+//  Check_In_HostTests.m
+//  Check-In HostTests
+//
+//  Created by Kaelin D. Hooper on 4/2/15.
+//  Copyright (c) 2015 CS378. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Check_In_HostTests : XCTestCase
+
+@end
+
+@implementation Check_In_HostTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
